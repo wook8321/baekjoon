a798717f92e0d02978df8ce6da9363bee645c51f
wook8321 <wook8321@knu.ac.kr>
2025-07-29 15:29:14 +0900

    가로수(gpt 도움 버전) + 주석 추가

--- Divisors, Multiples and Prime Numbers/2485.c
+++ Divisors, Multiples and Prime Numbers/2485.c
@@ -4,9 +4,9 @@ int gcd(int a, int b)
 {
     while(b != 0)
     {
-        int r = a % b;
-        a = b;
-        b = r;
+        int r = b;
+        b = a % b;
+        a = r;
     }
 
     return a;
@@ -20,18 +20,27 @@ int main()
 
     for(int i = 0; i < N; i++)
     {
-        scanf("%d", &colon[i]);
+        scanf("%d", &colon[i]);     // 가로수 위치 입력받기
     }
 
-    int g = colon[1] - colon[0];
-    int temp;
-    for(int j = 1; j < N - 1; j++)
+    int diff[N-1];
+ 
+    for(int j = 0; j < N - 1; j++)
     {
-        temp = colon[j+1] - colon[j];
-        g = gcd(g, temp);
+        diff[j] = colon[j+1] - colon[j];    // 가로수 간격 설정
     }
 
-    int min = ( (colon[N - 1] - colon[0]) / g ) - (N - 1);
+    int g = diff[0];
+    for(int k = 1; k < N - 1; k++)
+    {
+        g = gcd(g, diff[k]);        // gcd 비교를 통해 g에 가로수 간격들의 최대공약수 설정
+    }
+
+    int min = 0;                            // 초기화!! 꼭! (안해서 계속 틀림)
+    for(int m = 0; m < N - 1; m++)
+    {
+        min += (diff[m] / g - 1);       // 최소수는 간격들을 최대공약수로 나눈 값에 -1 한 값들을 모조리 더한 것이다.     
+    }
 
     printf("%d", min);
 
