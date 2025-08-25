1fd3d9a1fbdf53d3baf300b6a89f0116cc3c1935
wook8321 <wook8321@knu.ac.kr>
2025-08-25 11:08:23 +0900

    주석 추가

--- Stack,Queue,Deck/12789.c
+++ Stack,Queue,Deck/12789.c
@@ -4,12 +4,12 @@ int main()
 {
     int N;
     scanf("%d", &N);
-    int number[1000];    // 입력받는 학생들의 번호표
-    int stack[1000];     // 한 명씩 설 수 있는 공간
+    int number[1000];   // 입력받는 학생들의 번호표
+    int stack[1000];    // 한 명씩 설 수 있는 공간
 
     int top = -1;
 
-    int target = 1;   // 
+    int target = 1;     // 간식 받을 학생
     for(int i = 0; i < N; i++)
     {
         scanf("%d", &number[i]);
@@ -18,6 +18,7 @@ int main()
         {
             target++;
 
+            // 한 명이 간식을 받고 한 명씩 설 수 있는 공간에 다음 간식 받을 학생이 있을 경우
             while(top >= 0 && stack[top] == target)
             {
                 top--;
@@ -31,6 +32,7 @@ int main()
         }
     }
 
+    // 현재 줄에는 아무도 없고, 한 명씩만 서 있는 공간에 학생이 남은 경우
     while(top >= 0 && stack[top] == target)
     {
         target++;
