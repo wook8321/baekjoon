0c3b8a0b9075cd81e2c7dcc58b0cefd4cabb9490
wook8321 <wook8321@knu.ac.kr>
2025-10-02 16:12:17 +0900

    다리 놓기(overflow 문제 도움)

--- Combinatorics/1010.c
+++ Combinatorics/1010.c
@@ -10,14 +10,26 @@ int main()
         int N, M;
         scanf("%d %d", &N, &M);
 
-        int a = 1, b = 1;   // a는 분모, b는 분지
-        for(int j = M; j >= M - N + 1; j--)
+        /*
+        long long int a = 1, b = 1;  a는 분모, b는 분자
+        
+        for(int j = N; j > 0; j--)
         {
-            a *= j;
-            b *= j - M + N;
+            a *= j - N + M;
+            b *= j;
         }
 
-        printf("%d\n", a / b);
+        이렇게 분자와 분모를 각각 계속 곱해가면 overflow 발생
+        => 분모에 분자를 바로바로 나누어준다.
+        */
+        long long res = 1;
+
+        for(int j = 0; j < N; j++)
+        {
+            res = res * (M - j) / (j + 1);
+        }
+
+        printf("%lld\n", res);
     }
 
     return 0;
